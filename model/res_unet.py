0c33f252350b7377a0eaa9a2cfea13bc5fd1a4c6
Murad Bagamaev <murad.bagamaev@stud.cs.msu.ru>
2024-03-18 15:13:28 +0300

    Add EncoderBlock

--- model/res_unet.py
+++ model/res_unet.py
@@ -46,8 +46,47 @@ class ConvRes(nn.Module):
         return x + s
 
 
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, BN: bool = True):
+        super().__init__()
+
+        self.conv_res = ConvRes(in_channels, out_channels, BN)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv_res(inputs)
+        p = self.pool(x)
+        return x, p
+
+
+# class DecoderBlock(nn.Module):
+#     def __init__(self, in_channels: int, out_channels: int, BN: bool = True):
+#         super().__init__()
+
+#         self.upsample = nn.ConvTranspose2d(
+#             in_channels, out_channels, kernel_size=2, stride=2, padding="same"
+#         )
+#         self.conv = ConvRes(in_channels, out_channels, BN)
+
+#     def forward(self, inputs, shortcut):
+#         x = self.upsample(inputs)
+#         print(x.shape, shortcut.shape)
+
+
 class ResUNet(nn.Module):
     def __init__(
         self, n_classes: int, n_channels: int = 3, filters: int = 16, BN: bool = True
     ):
         super().__init__()
+
+        """ Encoder """
+        self.en1 = EncoderBlock(n_channels, filters, BN)
+
+    def forward(self, inputs):
+        x, p = self.en1(inputs)
+
+
+if __name__ == "__main__":
+    inputs = torch.randn([4, 3, 256, 256])
+    model = ResUNet(10)
+    model(inputs)
