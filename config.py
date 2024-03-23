d89e51801f1d6c6739deec5e9e0509c1eda81d52
Murad Bagamaev <murad.bagamaev@stud.cs.msu.ru>
2024-03-23 14:04:18 +0300

    Add one hot encoding function

---
+++ config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+LUMENSTONE_PATH = Path.home() / "dev_data" / "LumenStone" / "S1_v1"
+
+CLASS_NAMES = [
+    "BG",
+    "Ccp",
+    "Gl",
+    "Mag",
+    "Brt",
+    "Po",
+    "Py/Mrc",
+    "Pn",
+    "Sph",
+    "Apy",
+    "Hem",
+    "Tnt/Ttr",
+    "Kvl",
+]
