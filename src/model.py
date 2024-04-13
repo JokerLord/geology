a6cd2801fcd39f454b42482059252523c7f3f480
Murad Bagamaev <murad.bagamaev@stud.cs.msu.ru>
2024-04-13 23:18:09 +0300

    Add training and validation steps

--- src/model.py
+++ src/model.py
@@ -7,8 +7,9 @@ from torch import Tensor
 
 from res_unet import ResUNet
 from dataset import LumenStoneDataset
-from utils import split_into_patches, combine_from_patches
+from utils import split_into_patches, combine_from_patches, one_hot
 from config import *
+from metrics import iou_per_class, accuracy
 
 import random
 from sklearn.model_selection import train_test_split
@@ -30,60 +31,111 @@ class Trainer:
             root_dir=LUMENSTONE_PATH, train=True, transform=VAL_TRANSFORM
         )
 
+
     def _split_dataset(self) -> tuple[DataLoader, DataLoader]:
         indices = list(range(len(self._train_dataset)))
         train_indices, val_indices = train_test_split(
-            indices, train_size=SPLIT_RATIO, shuffle=True)
+            indices, train_size=SPLIT_RATIO, shuffle=True
+        )
         train_dataset = Subset(self._train_dataset, train_indices)
         val_dataset = Subset(self._val_dataset, val_indices)
         train_dataloader = DataLoader(train_dataset, batch_size=1, num_workers=4)
         val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=4)
         return train_dataloader, val_dataloader
 
-    def _train_one_image(self, inputs: Tensor, target: Tensor) -> float:
-        """
-        Arguments:
-            inputs: Input image of size (3, H, W)
-            target: Image mask of size (H, W)
-        Returns:
-            image_loss (float): mean loss for the whole image
-        """
 
-        image_loss = 0
-        input_patches = split_into_patches(
-            inputs,
-            patch_size=PATCH_SIZE,
-            offset=OFFSET,
-            overlap=PATCH_OVERLAP
+    def _train_image(self, batch: tuple[Tensor, Tensor]) -> dict[str, float]:
+        inputs, target = batch # (1, 3, H, W), (1, H, W)
+        inputs, target = torch.squeeze(inputs), torch.squeeze(target) # (3, H, W), (H, W)
+
+        inputs_patches = split_into_patches(
+            image=inputs, patch_size=PATCH_SIZE, offset=OFFSET, overlap=PATCH_OVERLAP
         )
         target_patches = split_into_patches(
-            target,
-            patch_size=PATCH_SIZE,
-            offset=OFFSET,
-            overlap=PATCH_OVERLAP
+            image=target, patch_size=PATCH_SIZE, offset=OFFSET, overlap=PATCH_OVERLAP
         )
 
         """ Make total number of patches a multiple of BATCH_SIZE """
-        while (len(input_patches) % BATCH_SIZE != 0):
-            input_patches.append(input_patches[-1])
+        while len(inputs_patches) % BATCH_SIZE != 0:
+            inputs_patches.append(inputs_patches[-1])
             target_patches.append(target_patches[-1])
 
         """ Shuffle patches """
-        zipped_patches = list(zip(input_patches, target_patches))
+        zipped_patches = list(zip(inputs_patches, target_patches))
         random.shuffle(zipped_patches)
-        input_patches, target_patches = zip(*zipped_patches)
+        inputs_patches, target_patches = zip(*zipped_patches)
 
-        for i in range(0, len(input_patches), BATCH_SIZE):
-            batch = torch.stack(input_patches[i: i + BATCH_SIZE]).to(device=self.device, dtype=torch.float32)
-            logits_batch = self.model(batch)
-            target_batch = torch.stack(target_patches[i: i + BATCH_SIZE]).to(device=self.device, dtype=torch.int64)
+        losses = []
+        for i in range(0, len(inputs_patches), BATCH_SIZE):
+            inputs_batch = torch.stack(inputs_patches[i : i + BATCH_SIZE]).to(
+                device=self.device, dtype=torch.float32
+            ) # (batch_size, 3, patch_size, patch_size)
+            logits_batch = self.model(inputs_batch)  # (batch_size, n_classes, patch_size, patch_size)
+            target_batch = torch.stack(target_patches[i : i + BATCH_SIZE]).to(
+                device=self.device, dtype=torch.int64
+            )  # (batch_size, patch_size, patch_size)
             loss = self.criterion(logits_batch, target_batch)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-            image_loss += loss.item()
+            losses.append(loss.item)
+        avg_loss = sum(losses) / len(losses)
+        return {"train_loss": avg_loss}
 
-        return image_loss / (len(input_patches) // BATCH_SIZE)
+    
+    def _validate_image(self, batch: tuple[Tensor, Tensor]) -> dict[str, float]:
+        inputs, target = batch # (1, 3, H, W), (1, H, W)
+        inputs, target = torch.squeeze(inputs), torch.squeeze(target) # (3, H, W), (H, W)
+
+        inputs_patches = split_into_patches(
+            image=inputs, patch_size=PATCH_SIZE, offset=OFFSET, overlap=PATCH_OVERLAP
+        )
+        target_patches = split_into_patches(
+            image=target, patch_size=PATCH_SIZE, offset=OFFSET, overlap=PATCH_OVERLAP
+        )
+
+        """ Make total number of patches a multiple of BATCH_SIZE """
+        init_n_patches = len(inputs_patches)
+        while len(inputs_patches) % BATCH_SIZE != 0:
+            inputs_patches.append(inputs_patches[-1])
+            target_patches.append(target_patches[-1])
+
+        with torch.no_grad():
+            logits_patches = []
+            losses = []
+            for i in range(0, len(inputs_patches), BATCH_SIZE):
+                inputs_batch = torch.stack(inputs_patches[i : i + BATCH_SIZE]).to(
+                    device=self.device, dtype=torch.float32
+                ) # (batch_size, 3, patch_size, patch_size)
+                logits_batch = self.model(inputs_batch)  # (batch_size, n_classes, patch_size, patch_size)
+                target_batch = torch.stack(target_patches[i: i + BATCH_SIZE]).to(
+                    device=self.device, dtype=torch.int64
+                ) # (batch_size, patch_size, patch_size)
+
+                loss = self.criterion(logits_batch, target_batch)
+                losses.append(loss.item())
+
+                for logits_patch in logits_batch:
+                    logits_patches.append(logits_patch)
+        avg_loss = sum(losses) / len(losses)
+
+        logits_patches = logits_patches[:init_n_patches]
+        logits = combine_from_patches(
+            logits_patches,
+            patch_size=PATCH_SIZE,
+            offset=OFFSET,
+            overlap=PATCH_OVERLAP,
+            src_shape=inputs.shape[-2:],
+        ) # (n_classes, H, W) in CPU memory
+    
+        activated = F.softmax(input=logits, dim=1) # (n_classes, H, W)
+        y_true = one_hot(target, n_classes=len(CLASS_NAMES))
+        iou_per_class(y_true, activated)
+
+        mask = torch.argmax(activated, dim=0) # (H, W)
+        prediction = one_hot(mask, n_classes=len(CLASS_NAMES))
+
+        
 
     def fit(self):
         self.model.to(self.device)
@@ -91,35 +143,11 @@ class Trainer:
             train_dataloader, val_dataloader = self._split_dataset()
 
             self.model.train()
-            train_loss = 0
+            train_outputs = []
             for batch in train_dataloader:
-                inputs, target = batch
-                image_loss = self._train_one_image(inputs[0], target[0])
-                train_loss += image_loss
-            train_loss = train_loss / len(train_dataloader)
+                train_outputs.append(self._train_image(batch))
 
-            with torch.no_grad():
-                for batch in val_dataloader:
-                    inputs, target = batch
-            print(f"Epoch {epoch}: train_loss: {train_loss:.3f}")
-
-                    # logits_patches = logits_patches[:init_n_patches]
-
-                    # logits = combine_from_patches(
-                    #     logits_patches,
-                    #     patch_size=PATCH_SIZE,
-                    #     offset=OFFSET,
-                    #     overlap=PATCH_OVERLAP,
-                    #     src_shape=inputs.shape[-2:],
-                    # )
-
-                    # logits = logits[None, ...]
-                    # target = target[None, ...]
-
-                    # activated = F.softmax(input=logits, dim=1)
-                    # predictions = torch.argmax(activated, dim=1)
-
-                    # loss = self.criterion(logits, target.to(torch.int64))
-                    # self.optimizer.zero_grad()
-                    # loss.backward()
-                    # self.optimizer.step()
+            self.model.eval()
+            val_outputs = []
+            for batch in val_dataloader:
+                val_outputs.append(self._validate_image(batch))
