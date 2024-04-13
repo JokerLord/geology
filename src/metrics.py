2294276a1688b2d39f12023f31ee660da758487a
Murad Bagamaev <murad.bagamaev@stud.cs.msu.ru>
2024-04-13 04:34:06 +0300

    Add accuracy metric

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,6 @@
 import torch
 from torch import Tensor
 
-from collections import namedtuple
-
 
 def iou(y_true: Tensor, y_pred: Tensor, smooth: float = 1.0) -> float:
     """
@@ -29,9 +27,24 @@ def iou_per_class(y_true: Tensor, y_pred: Tensor, smooth: float = 1.0) -> list[f
         smooth (float, Optional): Smooth coefficient. Default: 1.0
 
     Returns:
-        iou_vals (list[IoU]): List of IoU metrics per each of n_classes classes
+        iou_vals (list[float]): List of IoU metrics per each of n_classes classes
     """
     iou_vals = []
     for i in range(y_true.shape[0]):
         iou_vals.append(iou(y_true[i], y_pred[i], smooth))
     return iou_vals
+
+
+def accuracy(y_true: Tensor, y_pred: Tensor) -> float:
+    """
+    Arguments:
+        y_true (Tensor): One hot encoded tensor of size (n_classes, H, W)
+        y_pred (Tensor): Prediction tensor of size (n_classes, H, W)
+
+    Returns:
+        accuracy (float): Accuracy metric
+    """
+    y_true_a = torch.argmax(y_true, dim=0)
+    y_pred_a = torch.argmax(y_pred, dim=0)
+    correct = torch.sum(y_true_a == y_pred_a)
+    return correct.item() / torch.numel(y_true_a)
