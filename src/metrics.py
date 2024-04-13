1a8509c66167aff5a9b8eeaa3957d72da48fb316
Murad Bagamaev <murad.bagamaev@stud.cs.msu.ru>
2024-04-13 04:24:32 +0300

    Change iou and iou_per_class return values

--- src/metrics.py
+++ src/metrics.py
@@ -4,10 +4,24 @@ from torch import Tensor
 from collections import namedtuple
 
 
-iou_extended = namedtuple("IoU", ["iou", "intersection", "union"])
+def iou(y_true: Tensor, y_pred: Tensor, smooth: float = 1.0) -> float:
+    """
+    Arguments:
+        y_true (Tensor): Ground truth tensor of size (H, W)
+        y_pred (Tensor): Prediction tensor of size (H, W)
+        smooth (float, Optional): Smooth coefficient. Default: 1.0
+
+    Returns:
+        iou (float): Calculated IoU metric
+    """
+    y_true_f = torch.flatten(y_true)
+    y_pred_f = torch.flatten(y_pred)
+    intersection = torch.sum(y_true_f * y_pred_f)
+    union = torch.sum(y_true_f) + torch.sum(y_pred_f) - intersection
+    return (intersection + smooth).item() / (union + smooth).item()
 
 
-def iou(y_true: Tensor, y_pred: Tensor, smooth=1.0) -> iou_extended:
+def iou_per_class(y_true: Tensor, y_pred: Tensor, smooth: float = 1.0) -> list[float]:
     """
     Arguments:
         y_true (Tensor): One hot encoded tensor of size (n_classes, H, W)
@@ -15,12 +29,9 @@ def iou(y_true: Tensor, y_pred: Tensor, smooth=1.0) -> iou_extended:
         smooth (float, Optional): Smooth coefficient. Default: 1.0
 
     Returns:
-        iou_extended (IoU): Calculated IoU metric with intersection and
-            union for joint iou calculation
+        iou_vals (list[IoU]): List of IoU metrics per each of n_classes classes
     """
-    y_true_f = torch.flatten(y_true)
-    y_pred_f = torch.flatten(y_pred)
-    intersection = torch.sum(y_true_f * y_pred_f)
-    union = torch.sum(y_true_f) + torch.sum(y_pred_f) - intersection
-    iou = (intersection + smooth) / (union + smooth)
-    return iou_extended(iou, intersection, union)
+    iou_vals = []
+    for i in range(y_true.shape[0]):
+        iou_vals.append(iou(y_true[i], y_pred[i], smooth))
+    return iou_vals
